ef50d48c1d36c1642f1bc665bafb8a889bdfca71
hc <harshalchavan170@gmail.com>
2025-01-08 00:27:52 +0530

    Logic for app.js changed

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Signup from "./Components/Signup";
 import Login from "./Components/Login";
+import Home from "./Page/Home";
 
 const App = () => (
   <Router>
     <Routes>
+      <Route path="/" element={<Home />} />
       <Route path="/signup" element={<Signup />} />
       <Route path="/login" element={<Login />} />
     </Routes>
